26735eb293f1d2c255195d468b56d6d0de4ef61f
Tansgo <bartholome84@gmail.com>
2022-07-03 23:48:04 +0200

    Ajout d'un range pour la longueur du mot de passe

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const password_text = document.getElementById("password_text");
 const generator_button = document.getElementById("generator_button");
+const password_length_range = document.getElementById("length_range");
+const password_length_number = document.getElementById("length_number");
 
 const letters = "abcdefghilkmnopqrstuvwxyz";
 const numbers = "0123456789";
 const special_characters = "&é(-è_çà)=^$ù*,;:!#{[]}?.//§%µ¨£+<>"
 
-function generator_new_password(){
+let password_length = password_length_number.value;
+
+function generator_new_password() {
     let password_string = "";
-    for(let i = 0; i < 30; i++){
-        switch(parseInt(Math.random() * 4)){
+
+    for (let i = 0; i < password_length; i++) {
+        switch (parseInt(Math.random() * 4)) {
             case 1:
                 password_string += letters[parseInt(Math.random() * letters.length)].toUpperCase();
                 break;
@@ -24,5 +29,15 @@ function generator_new_password(){
     }
     password_text.value = password_string;
 }
+
+function update_length_range() {
+    //On met à jour tout les input concernant la longueur du mot de passe en vérifiant quelle valeur a changé
+    password_length === password_length_number.value ?
+        password_length = password_length_range.value : password_length = password_length_number.value;
+    password_length_number.value = password_length_range.value = password_length;
+}
+
+password_length_range.onchange = update_length_range;
+password_length_number.onchange = update_length_range;
 
 generator_button.onclick = generator_new_password;
